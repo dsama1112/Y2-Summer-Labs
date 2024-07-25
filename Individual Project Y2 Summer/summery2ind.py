0ea002732285c6651c821ef964ec777d9d07dfba
Sama <dsama1112dar@gmail.com>
2024-07-25 10:41:58 +0300

    Finished project, need to do sign out and delete comments + add design for visuals

--- Individual Project Y2 Summer/summery2ind.py
+++ Individual Project Y2 Summer/summery2ind.py
@@ -3,8 +3,6 @@ from flask import session as session
 import pyrebase
 
 
-
-
 Config = {
   "apiKey": "AIzaSyCz_lkkxNcF3Hfcuri5jx_m6AHnSQnHmQE",
   "authDomain": "y2-individual-summer-project.firebaseapp.com",
@@ -35,10 +33,10 @@ def signup():
   			session['user'] = auth.create_user_with_email_and_password(email, password)
   			user={"email": email, "password": password}
   			UID = session['user']['localId']
-  			db.child("users").child(UID).set(user)
-  			db.child("recipe").set({"Name": "Milkshake", 
-  				"Ingredients": "1/4 cup milk, 3 tablespoons sugar, 2 cups halved fresh strawberries, 1-3/4 cups sliced peeled peaches (about 3 medium) or frozen unsweetened sliced peaches, thawed, 2 cups vanilla ice cream", 
-  				"Instructions": "...."})
+  			# db.child("users").child(UID).set(user)
+  			# db.child("recipe").set({"Name": "Milkshake", 
+  			# 	"Ingredients": "1/4 cup milk, 3 tablespoons sugar, 2 cups halved fresh strawberries, 1-3/4 cups sliced peeled peaches (about 3 medium) or frozen unsweetened sliced peaches, thawed, 2 cups vanilla ice cream", 
+  			# 	"Instructions": "...."})
   			return redirect(url_for('category'))
   		except:
   			return "Authentication failed"
@@ -50,7 +48,6 @@ def signin():
 	error=""
 	if request.method == 'POST':
 		try:
-			username = request.form['username']
 			email = request.form['email']
 			password = request.form['password']
 			session['user'] = auth.sign_in_with_email_and_password(email, password)
@@ -62,17 +59,58 @@ def signin():
 
 @app.route('/category', methods=['GET', 'POST'])
 def category():
-	error=""
-	if request.method == 'GET':
-		return render_template("category.html")
-	else:
-		return render_template("category1.html")
+	return render_template("category.html")
 
 
 @app.route('/category1', methods=['GET', 'POST'])
 def category1():
+	error=""
+	# show_rec= db.child("recipe").get().val()
+	if request.method == 'POST':
+		return render_template("category1.html")
 	return render_template("category1.html")
 
-    
+
+@app.route('/category2', methods=['GET', 'POST'])
+def category2():
+	error=""
+	show_rec= db.child("recipe").get().val()
+	if request.method == 'POST':
+		print(show_rec)
+		return render_template("category2.html", show_rec=show_rec)
+	return render_template("category2.html", show_rec=show_rec)
+
+
+@app.route('/category3', methods=['GET', 'POST'])
+def category3():
+	error=""
+	show_rec= db.child("recipe").get().val()
+	if request.method == 'POST':
+		print(show_rec)
+		return render_template("category3.html", show_rec=show_rec)
+	return render_template("category3.html", show_rec=show_rec)
+
+
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+	error=""
+	if request.method == 'POST':
+		comment = request.form['comment']
+		comment_from = request.form['comment_from']
+		UID = session['user']['localId']
+		comments = {"Name:": comment_from, "Comment:": comment}
+		db.child("comments").push(comments)
+		return redirect(url_for('review'))
+	return render_template("review.html")
+
+
+@app.route('/view', methods=['GET', 'POST'])
+def view():
+	error=""
+	show_comments= db.child("comments").get().val()
+	print(show_comments)
+	return render_template("view.html", show_comments=show_comments)
+		
+
 if __name__ == '__main__':
 	app.run(debug=True)
